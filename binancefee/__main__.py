03f184007e885395e91a31e9f71671d1ea939f63
Eugene The Red <me@eugenethered.com>
2022-09-03 18:57:53 +0100

    using env var feature

--- binancefee/__main__.py
+++ binancefee/__main__.py
@@ -2,9 +2,8 @@ import logging
 
 from cache.holder.RedisCacheHolder import RedisCacheHolder
 from cache.provider.RedisCacheProviderWithHash import RedisCacheProviderWithHash
-from core.arguments.command_line_arguments import option_arg_parser
+from core.environment.EnvironmentVariables import EnvironmentVariables
 from logger.ConfigureLogger import ConfigureLogger
-from metainfo.MetaInfo import MetaInfo
 
 from binancefee.BinanceFeeConductor import BinanceFeeConductor
 
@@ -12,17 +11,14 @@ from binancefee.BinanceFeeConductor import BinanceFeeConductor
 def start():
     ConfigureLogger()
 
-    meta_info = MetaInfo('persuader-technology-automata-fee-conductor-binance')
-
-    command_line_arg_parser = option_arg_parser(meta_info)
-    args = command_line_arg_parser.parse_args()
+    environment_variables = EnvironmentVariables()
 
     log = logging.getLogger('Binance Fee Conductor')
     log.info('fee conductor initialized')
 
-    RedisCacheHolder(args.options, held_type=RedisCacheProviderWithHash)
+    RedisCacheHolder(environment_variables.options, held_type=RedisCacheProviderWithHash)
 
-    conductor = BinanceFeeConductor(args.options)
+    conductor = BinanceFeeConductor(environment_variables.options)
     conductor.start_process_schedule()
 
 
